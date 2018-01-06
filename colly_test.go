e9e9fcbe34eaeac8fe4e5f042c113b310335f007
happycamper <happycamper.sw@gmail.com>
2018-01-06 21:30:19 +0100

    Use *httptest.Server in tests, remove init func

--- colly_test.go
+++ colly_test.go
@@ -3,9 +3,8 @@ package colly
 import (
 	"bytes"
 	"fmt"
-	"log"
-	"net"
 	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"regexp"
 	"strings"
@@ -16,9 +15,6 @@ import (
 	"github.com/gocolly/colly/debug"
 )
 
-var testServerPort = 31337
-var testServerAddr = fmt.Sprintf("127.0.0.1:%d", testServerPort)
-var testServerRootURL = fmt.Sprintf("http://%s/", testServerAddr)
 var serverIndexResponse = []byte("hello world\n")
 var robotsFile = `
 User-agent: *
@@ -26,18 +22,14 @@ Allow: /allowed
 Disallow: /disallowed
 `
 
-func init() {
-	srv := &http.Server{}
-	listener, err := net.ListenTCP("tcp4", &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: testServerPort})
-	if err != nil {
-		panic(err)
-	}
+func newTestServer() *httptest.Server {
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write(serverIndexResponse)
 	})
 
-	http.HandleFunc("/html", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/html", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Conent-Type", "text/html")
 		w.Write([]byte(`<!DOCTYPE html>
 <html>
@@ -53,44 +45,45 @@ func init() {
 		`))
 	})
 
-	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			w.Header().Set("Conent-Type", "text/html")
 			w.Write([]byte(r.FormValue("name")))
 		}
 	})
 
-	http.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Write([]byte(robotsFile))
 	})
 
-	http.HandleFunc("/allowed", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/allowed", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Write([]byte("allowed"))
 	})
 
-	http.HandleFunc("/disallowed", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/disallowed", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Write([]byte("disallowed"))
 	})
 
-	http.Handle("/redirect", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/redirect", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/redirected/", http.StatusSeeOther)
 
 	}))
-	http.Handle("/redirected/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	mux.Handle("/redirected/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, `<a href="test">test</a>`)
 	}))
 
-	http.HandleFunc("/set_cookie", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/set_cookie", func(w http.ResponseWriter, r *http.Request) {
 		c := &http.Cookie{Name: "test", Value: "testv", HttpOnly: false}
 		http.SetCookie(w, c)
 		w.WriteHeader(200)
 		w.Write([]byte("ok"))
 	})
 
-	http.HandleFunc("/check_cookie", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/check_cookie", func(w http.ResponseWriter, r *http.Request) {
 		cs := r.Cookies()
 		if len(cs) != 1 || r.Cookies()[0].Value != "testv" {
 			w.WriteHeader(500)
@@ -101,164 +94,158 @@ func init() {
 		w.Write([]byte("ok"))
 	})
 
-	go func() {
-		if err := srv.Serve(listener); err != nil {
-			log.Printf("Httpserver: ListenAndServe() error: %s", err)
+	return httptest.NewServer(mux)
+}
+
+var newCollectorTests = map[string]func(*testing.T){
+	"UserAgent": func(t *testing.T) {
+		for _, ua := range []string{
+			"foo",
+			"bar",
+		} {
+			c := NewCollector(UserAgent(ua))
+
+			if got, want := c.UserAgent, ua; got != want {
+				t.Fatalf("c.UserAgent = %q, want %q", got, want)
+			}
 		}
-	}()
+	},
+	"MaxDepth": func(t *testing.T) {
+		for _, depth := range []int{
+			12,
+			34,
+			0,
+		} {
+			c := NewCollector(MaxDepth(depth))
+
+			if got, want := c.MaxDepth, depth; got != want {
+				t.Fatalf("c.MaxDepth = %d, want %d", got, want)
+			}
+		}
+	},
+	"AllowedDomains": func(t *testing.T) {
+		for _, domains := range [][]string{
+			{"example.com", "example.net"},
+			{"example.net"},
+			{},
+			nil,
+		} {
+			c := NewCollector(AllowedDomains(domains...))
+
+			if got, want := c.AllowedDomains, domains; !reflect.DeepEqual(got, want) {
+				t.Fatalf("c.AllowedDomains = %q, want %q", got, want)
+			}
+		}
+	},
+	"DisallowedDomains": func(t *testing.T) {
+		for _, domains := range [][]string{
+			{"example.com", "example.net"},
+			{"example.net"},
+			{},
+			nil,
+		} {
+			c := NewCollector(DisallowedDomains(domains...))
+
+			if got, want := c.DisallowedDomains, domains; !reflect.DeepEqual(got, want) {
+				t.Fatalf("c.DisallowedDomains = %q, want %q", got, want)
+			}
+		}
+	},
+	"URLFilters": func(t *testing.T) {
+		for _, filters := range [][]*regexp.Regexp{
+			{regexp.MustCompile(`\w+`)},
+			{regexp.MustCompile(`\d+`)},
+			{},
+			nil,
+		} {
+			c := NewCollector(URLFilters(filters...))
+
+			if got, want := c.URLFilters, filters; !reflect.DeepEqual(got, want) {
+				t.Fatalf("c.URLFilters = %v, want %v", got, want)
+			}
+		}
+	},
+	"AllowURLRevisit": func(t *testing.T) {
+		c := NewCollector(AllowURLRevisit())
+
+		if !c.AllowURLRevisit {
+			t.Fatal("c.AllowURLRevisit = false, want true")
+		}
+	},
+	"MaxBodySize": func(t *testing.T) {
+		for _, sizeInBytes := range []int{
+			1024 * 1024,
+			1024,
+			0,
+		} {
+			c := NewCollector(MaxBodySize(sizeInBytes))
+
+			if got, want := c.MaxBodySize, sizeInBytes; got != want {
+				t.Fatalf("c.MaxBodySize = %d, want %d", got, want)
+			}
+		}
+	},
+	"CacheDir": func(t *testing.T) {
+		for _, path := range []string{
+			"/tmp/",
+			"/var/cache/",
+		} {
+			c := NewCollector(CacheDir(path))
+
+			if got, want := c.CacheDir, path; got != want {
+				t.Fatalf("c.CacheDir = %q, want %q", got, want)
+			}
+		}
+	},
+	"IgnoreRobotsTxt": func(t *testing.T) {
+		c := NewCollector(IgnoreRobotsTxt())
+
+		if !c.IgnoreRobotsTxt {
+			t.Fatal("c.IgnoreRobotsTxt = false, want true")
+		}
+	},
+	"ID": func(t *testing.T) {
+		for _, id := range []uint32{
+			0,
+			1,
+			2,
+		} {
+			c := NewCollector(ID(id))
+
+			if got, want := c.ID, id; got != want {
+				t.Fatalf("c.ID = %d, want %d", got, want)
+			}
+		}
+	},
+	"DetectCharset": func(t *testing.T) {
+		c := NewCollector(DetectCharset())
+
+		if !c.DetectCharset {
+			t.Fatal("c.DetectCharset = false, want true")
+		}
+	},
+	"Debugger": func(t *testing.T) {
+		d := &debug.LogDebugger{}
+		c := NewCollector(Debugger(d))
+
+		if got, want := c.debugger, d; got != want {
+			t.Fatalf("c.debugger = %v, want %v", got, want)
+		}
+	},
 }
 
 func TestNewCollector(t *testing.T) {
 	t.Run("Functional Options", func(t *testing.T) {
-		t.Run("UserAgent", func(t *testing.T) {
-			for _, ua := range []string{
-				"foo",
-				"bar",
-			} {
-				c := NewCollector(UserAgent(ua))
-
-				if got, want := c.UserAgent, ua; got != want {
-					t.Fatalf("c.UserAgent = %q, want %q", got, want)
-				}
-			}
-		})
-
-		t.Run("MaxDepth", func(t *testing.T) {
-			for _, depth := range []int{
-				12,
-				34,
-				0,
-			} {
-				c := NewCollector(MaxDepth(depth))
-
-				if got, want := c.MaxDepth, depth; got != want {
-					t.Fatalf("c.MaxDepth = %d, want %d", got, want)
-				}
-			}
-		})
-
-		t.Run("AllowedDomains", func(t *testing.T) {
-			for _, domains := range [][]string{
-				[]string{"example.com", "example.net"},
-				[]string{"example.net"},
-				[]string{},
-				nil,
-			} {
-				c := NewCollector(AllowedDomains(domains...))
-
-				if got, want := c.AllowedDomains, domains; !reflect.DeepEqual(got, want) {
-					t.Fatalf("c.AllowedDomains = %q, want %q", got, want)
-				}
-			}
-		})
-
-		t.Run("DisallowedDomains", func(t *testing.T) {
-			for _, domains := range [][]string{
-				[]string{"example.com", "example.net"},
-				[]string{"example.net"},
-				[]string{},
-				nil,
-			} {
-				c := NewCollector(DisallowedDomains(domains...))
-
-				if got, want := c.DisallowedDomains, domains; !reflect.DeepEqual(got, want) {
-					t.Fatalf("c.DisallowedDomains = %q, want %q", got, want)
-				}
-			}
-		})
-
-		t.Run("URLFilters", func(t *testing.T) {
-			for _, filters := range [][]*regexp.Regexp{
-				[]*regexp.Regexp{regexp.MustCompile(`\w+`)},
-				[]*regexp.Regexp{regexp.MustCompile(`\d+`)},
-				[]*regexp.Regexp{},
-				nil,
-			} {
-				c := NewCollector(URLFilters(filters...))
-
-				if got, want := c.URLFilters, filters; !reflect.DeepEqual(got, want) {
-					t.Fatalf("c.URLFilters = %v, want %v", got, want)
-				}
-			}
-		})
-
-		t.Run("AllowURLRevisit", func(t *testing.T) {
-			c := NewCollector(AllowURLRevisit())
-
-			if !c.AllowURLRevisit {
-				t.Fatal("c.AllowURLRevisit = false, want true")
-			}
-		})
-
-		t.Run("MaxBodySize", func(t *testing.T) {
-			for _, sizeInBytes := range []int{
-				1024 * 1024,
-				1024,
-				0,
-			} {
-				c := NewCollector(MaxBodySize(sizeInBytes))
-
-				if got, want := c.MaxBodySize, sizeInBytes; got != want {
-					t.Fatalf("c.MaxBodySize = %d, want %d", got, want)
-				}
-			}
-		})
-
-		t.Run("CacheDir", func(t *testing.T) {
-			for _, path := range []string{
-				"/tmp/",
-				"/var/cache/",
-			} {
-				c := NewCollector(CacheDir(path))
-
-				if got, want := c.CacheDir, path; got != want {
-					t.Fatalf("c.CacheDir = %q, want %q", got, want)
-				}
-			}
-		})
-
-		t.Run("IgnoreRobotsTxt", func(t *testing.T) {
-			c := NewCollector(IgnoreRobotsTxt())
-
-			if !c.IgnoreRobotsTxt {
-				t.Fatal("c.IgnoreRobotsTxt = false, want true")
-			}
-		})
-
-		t.Run("ID", func(t *testing.T) {
-			for _, id := range []uint32{
-				0,
-				1,
-				2,
-			} {
-				c := NewCollector(ID(id))
-
-				if got, want := c.ID, id; got != want {
-					t.Fatalf("c.ID = %d, want %d", got, want)
-				}
-			}
-		})
-
-		t.Run("DetectCharset", func(t *testing.T) {
-			c := NewCollector(DetectCharset())
-
-			if !c.DetectCharset {
-				t.Fatal("c.DetectCharset = false, want true")
-			}
-		})
-
-		t.Run("Debugger", func(t *testing.T) {
-			d := &debug.LogDebugger{}
-			c := NewCollector(Debugger(d))
-
-			if got, want := c.debugger, d; got != want {
-				t.Fatalf("c.debugger = %v, want %v", got, want)
-			}
-		})
+		for name, test := range newCollectorTests {
+			t.Run(name, test)
+		}
 	})
 }
 
 func TestCollectorVisit(t *testing.T) {
+	ts := newTestServer()
+	defer ts.Close()
+
 	c := NewCollector()
 
 	onRequestCalled := false
@@ -294,7 +281,7 @@ func TestCollectorVisit(t *testing.T) {
 		onScrapedCalled = true
 	})
 
-	c.Visit(testServerRootURL)
+	c.Visit(ts.URL)
 
 	if !onRequestCalled {
 		t.Error("Failed to call OnRequest callback")
@@ -310,6 +297,9 @@ func TestCollectorVisit(t *testing.T) {
 }
 
 func TestCollectorOnHTML(t *testing.T) {
+	ts := newTestServer()
+	defer ts.Close()
+
 	c := NewCollector()
 
 	titleCallbackCalled := false
@@ -339,7 +329,7 @@ func TestCollectorOnHTML(t *testing.T) {
 		}
 	})
 
-	c.Visit(testServerRootURL + "html")
+	c.Visit(ts.URL + "/html")
 
 	if !titleCallbackCalled {
 		t.Error("Failed to call OnHTML callback for <title> tag")
@@ -351,6 +341,9 @@ func TestCollectorOnHTML(t *testing.T) {
 }
 
 func TestCollectorURLRevisit(t *testing.T) {
+	ts := newTestServer()
+	defer ts.Close()
+
 	c := NewCollector()
 
 	visitCount := 0
@@ -359,8 +352,8 @@ func TestCollectorURLRevisit(t *testing.T) {
 		visitCount++
 	})
 
-	c.Visit(testServerRootURL)
-	c.Visit(testServerRootURL)
+	c.Visit(ts.URL)
+	c.Visit(ts.URL)
 
 	if visitCount != 1 {
 		t.Error("URL revisited")
@@ -368,8 +361,8 @@ func TestCollectorURLRevisit(t *testing.T) {
 
 	c.AllowURLRevisit = true
 
-	c.Visit(testServerRootURL)
-	c.Visit(testServerRootURL)
+	c.Visit(ts.URL)
+	c.Visit(ts.URL)
 
 	if visitCount != 3 {
 		t.Error("URL not revisited")
@@ -377,6 +370,9 @@ func TestCollectorURLRevisit(t *testing.T) {
 }
 
 func TestCollectorPost(t *testing.T) {
+	ts := newTestServer()
+	defer ts.Close()
+
 	postValue := "hello"
 	c := NewCollector()
 
@@ -386,12 +382,15 @@ func TestCollectorPost(t *testing.T) {
 		}
 	})
 
-	c.Post(testServerRootURL+"login", map[string]string{
+	c.Post(ts.URL+"/login", map[string]string{
 		"name": postValue,
 	})
 }
 
 func TestRedirect(t *testing.T) {
+	ts := newTestServer()
+	defer ts.Close()
+
 	c := NewCollector()
 	c.OnHTML("a[href]", func(e *HTMLElement) {
 		u := e.Request.AbsoluteURL(e.Attr("href"))
@@ -399,31 +398,40 @@ func TestRedirect(t *testing.T) {
 			t.Error("Invalid URL after redirect: " + u)
 		}
 	})
-	c.Visit(testServerRootURL + "redirect")
+	c.Visit(ts.URL + "/redirect")
 }
 
 func TestCollectorCookies(t *testing.T) {
+	ts := newTestServer()
+	defer ts.Close()
+
 	c := NewCollector()
 
-	if err := c.Visit(testServerRootURL + "set_cookie"); err != nil {
+	if err := c.Visit(ts.URL + "/set_cookie"); err != nil {
 		t.Fatal(err)
 	}
 
-	if err := c.Visit(testServerRootURL + "check_cookie"); err != nil {
+	if err := c.Visit(ts.URL + "/check_cookie"); err != nil {
 		t.Fatalf("Failed to use previously set cookies: %s", err)
 	}
 }
 
 func BenchmarkVisit(b *testing.B) {
+	ts := newTestServer()
+	defer ts.Close()
+
 	c := NewCollector()
 	c.OnHTML("p", func(_ *HTMLElement) {})
 
 	for n := 0; n < b.N; n++ {
-		c.Visit(fmt.Sprintf("%shtml?q=%d", testServerRootURL, n))
+		c.Visit(fmt.Sprintf("%s/html?q=%d", ts.URL, n))
 	}
 }
 
 func TestRobotsWhenAllowed(t *testing.T) {
+	ts := newTestServer()
+	defer ts.Close()
+
 	c := NewCollector()
 	c.IgnoreRobotsTxt = false
 
@@ -433,7 +441,7 @@ func TestRobotsWhenAllowed(t *testing.T) {
 		}
 	})
 
-	err := c.Visit(testServerRootURL + "allowed")
+	err := c.Visit(ts.URL + "/allowed")
 
 	if err != nil {
 		t.Fatal(err)
@@ -441,6 +449,9 @@ func TestRobotsWhenAllowed(t *testing.T) {
 }
 
 func TestRobotsWhenDisallowed(t *testing.T) {
+	ts := newTestServer()
+	defer ts.Close()
+
 	c := NewCollector()
 	c.IgnoreRobotsTxt = false
 
@@ -448,13 +459,16 @@ func TestRobotsWhenDisallowed(t *testing.T) {
 		t.Fatalf("Received response: %d", resp.StatusCode)
 	})
 
-	err := c.Visit(testServerRootURL + "disallowed")
+	err := c.Visit(ts.URL + "/disallowed")
 	if err.Error() != "URL blocked by robots.txt" {
 		t.Fatalf("wrong error message: %v", err)
 	}
 }
 
 func TestIgnoreRobotsWhenDisallowed(t *testing.T) {
+	ts := newTestServer()
+	defer ts.Close()
+
 	c := NewCollector()
 	c.IgnoreRobotsTxt = true
 
@@ -464,7 +478,7 @@ func TestIgnoreRobotsWhenDisallowed(t *testing.T) {
 		}
 	})
 
-	err := c.Visit(testServerRootURL + "disallowed")
+	err := c.Visit(ts.URL + "/disallowed")
 
 	if err != nil {
 		t.Fatal(err)
