3f0eb2a901a140021ce34909584774b4cf0a4404
happycamper <happycamper.sw@gmail.com>
2018-10-04 11:47:13 -0600

    Add Jive Search

--- extensions/referrer.go
+++ extensions/referrer.go
@@ -4,16 +4,16 @@ import (
 	"github.com/gocolly/colly"
 )
 
-// Referer sets valid Referer HTTP header to requests.
+// Referrer sets valid Referrer HTTP header to requests.
 // Warning: this extension works only if you use Request.Visit
 // from callbacks instead of Collector.Visit.
-func Referer(c *colly.Collector) {
+func Referrer(c *colly.Collector) {
 	c.OnResponse(func(r *colly.Response) {
-		r.Ctx.Put("_referer", r.Request.URL.String())
+		r.Ctx.Put("_referrer", r.Request.URL.String())
 	})
 	c.OnRequest(func(r *colly.Request) {
-		if ref := r.Ctx.Get("_referer"); ref != "" {
-			r.Headers.Set("Referer", ref)
+		if ref := r.Ctx.Get("_referrer"); ref != "" {
+			r.Headers.Set("Referrer", ref)
 		}
 	})
 }
